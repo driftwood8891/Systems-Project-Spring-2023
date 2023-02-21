775b40bd7ceee8ac967ab913d78fd585cfabe857
driftwood8891 <95161085+driftwood8891@users.noreply.github.com>
2023-02-21 11:54:51 -0600

    Add project files.

---
+++ Systems-Project_Spring_2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Systems_Project_Spring_2023.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
