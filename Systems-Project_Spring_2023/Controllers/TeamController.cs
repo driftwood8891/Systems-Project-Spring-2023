ea814c8e8d90698929ed33484dd0ae5199ebdb38
Patrick Bailey <205156@mail.macc.edu>
2023-03-21 17:05:17 -0500

    Added Patrick Bailey

--- Systems-Project_Spring_2023/Controllers/TeamController.cs
+++ Systems-Project_Spring_2023/Controllers/TeamController.cs
@@ -28,5 +28,10 @@ namespace Systems_Project_Spring_2023.Controllers
 		{
 			return View();
 		}
-	}
+
+        public IActionResult PatrickBailey()
+        {
+            return View();
+        }
+    }
 }
