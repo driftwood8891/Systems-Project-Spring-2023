5530b262afd4736a8cd70c341dbb152c4b11cf60
driftwood8891 <95161085+driftwood8891@users.noreply.github.com>
2023-04-04 15:14:22 -0500

    Added properties to Status model

--- Systems-Project_Spring_2023/Models/Status.cs
+++ Systems-Project_Spring_2023/Models/Status.cs
@@ -1,6 +1,15 @@
-﻿namespace Systems_Project_Spring_2023.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Systems_Project_Spring_2023.Models
 {
 	public class Status
 	{
+		[Key] 
+		[StringLength(2)] 
+		[Required] 
+		public string Status_code { get; set; } = null!;
+
+		[StringLength(20)] 
+		public string Status_desc { get; set; } = null!;
 	}
 }
