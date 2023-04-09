0581cc25d949449758b592a8a43ff2e8d7d38e52
Nathan Cochran <nacochranpb@gmail.com>
2023-04-09 16:27:42 -0500

    Added Item/Kit Controllers

---
+++ Systems-Project_Spring_2023/Controllers/ItemsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Systems_Project_Spring_2023.Data;
+using Systems_Project_Spring_2023.Models;
+
+namespace Systems_Project_Spring_2023.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Items
+        public async Task<IActionResult> Index()
+        {
+              return _context.Items != null ? 
+                          View(await _context.Items.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Items'  is null.");
+        }
+
+        // GET: Items/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Items == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items
+                .FirstOrDefaultAsync(m => m.Item_id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // GET: Items/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Items/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Item_id,Item_barcode,Item_name,Item_cost,Item_date,Item_note,Status_code,Student_macid")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        // GET: Items/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Items == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+
+        // POST: Items/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Item_id,Item_barcode,Item_name,Item_cost,Item_date,Item_note,Status_code,Student_macid")] Item item)
+        {
+            if (id != item.Item_id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(item);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ItemExists(item.Item_id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        // GET: Items/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Items == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items
+                .FirstOrDefaultAsync(m => m.Item_id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Items == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Items'  is null.");
+            }
+            var item = await _context.Items.FindAsync(id);
+            if (item != null)
+            {
+                _context.Items.Remove(item);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ItemExists(string id)
+        {
+          return (_context.Items?.Any(e => e.Item_id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Systems-Project_Spring_2023/Controllers/KitsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Systems_Project_Spring_2023.Data;
+using Systems_Project_Spring_2023.Models;
+
+namespace Systems_Project_Spring_2023.Controllers
+{
+    public class KitsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public KitsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Kits
+        public async Task<IActionResult> Index()
+        {
+              return _context.Kits != null ? 
+                          View(await _context.Kits.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Kits'  is null.");
+        }
+
+        // GET: Kits/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Kits == null)
+            {
+                return NotFound();
+            }
+
+            var kit = await _context.Kits
+                .FirstOrDefaultAsync(m => m.Kit_id == id);
+            if (kit == null)
+            {
+                return NotFound();
+            }
+
+            return View(kit);
+        }
+
+        // GET: Kits/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kits/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Kit_id,Kit_barcd,Kit_name,Kit_qty,Kit_desc,Kit_cost,Kit_date,Kit_note,Kt_id,Status_code,Student_macid")] Kit kit)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(kit);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kit);
+        }
+
+        // GET: Kits/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Kits == null)
+            {
+                return NotFound();
+            }
+
+            var kit = await _context.Kits.FindAsync(id);
+            if (kit == null)
+            {
+                return NotFound();
+            }
+            return View(kit);
+        }
+
+        // POST: Kits/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Kit_id,Kit_barcd,Kit_name,Kit_qty,Kit_desc,Kit_cost,Kit_date,Kit_note,Kt_id,Status_code,Student_macid")] Kit kit)
+        {
+            if (id != kit.Kit_id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(kit);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KitExists(kit.Kit_id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kit);
+        }
+
+        // GET: Kits/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Kits == null)
+            {
+                return NotFound();
+            }
+
+            var kit = await _context.Kits
+                .FirstOrDefaultAsync(m => m.Kit_id == id);
+            if (kit == null)
+            {
+                return NotFound();
+            }
+
+            return View(kit);
+        }
+
+        // POST: Kits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Kits == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Kits'  is null.");
+            }
+            var kit = await _context.Kits.FindAsync(id);
+            if (kit != null)
+            {
+                _context.Kits.Remove(kit);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KitExists(string id)
+        {
+          return (_context.Kits?.Any(e => e.Kit_id == id)).GetValueOrDefault();
+        }
+    }
+}
