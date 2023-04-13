850b309fc9913b1afba30485f32cb8714167bc1b
Nathan Cochran <nacochranpb@gmail.com>
2023-04-13 18:27:17 -0500

    added migrations

---
+++ Systems-Project_Spring_2023/Data/Migrations/20230413201549_models.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Systems_Project_Spring_2023.Data.Migrations
+{
+    public partial class models : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "La_id",
+                table: "LabAssistants",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Item_qty",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Item_qty",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "La_id",
+                table: "LabAssistants",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
