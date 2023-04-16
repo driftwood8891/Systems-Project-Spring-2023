f66dafa4064fb86d30b52225f4ebfa833890153a
driftwood8891 <95161085+driftwood8891@users.noreply.github.com>
2023-04-16 01:03:52 -0500

    Added Checkin/CheckOut features.  Had to add JoinData class so we could select data from mulitple tables.

--- Systems-Project_Spring_2023/Controllers/HomeController.cs
+++ Systems-Project_Spring_2023/Controllers/HomeController.cs
@@ -3,17 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Systems_Project_Spring_2023.Models;
 using System.Web;
+using Systems_Project_Spring_2023.Data;
+using Microsoft.Extensions.Logging;
 
 namespace Systems_Project_Spring_2023.Controllers
 {
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly ApplicationDbContext _context;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ApplicationDbContext context, ILogger<HomeController> logger)
 		{
+			_context = context;
 			_logger = logger;
 		}
+
 
 		public IActionResult Index()
 		{
@@ -35,15 +40,75 @@ namespace Systems_Project_Spring_2023.Controllers
             return View();
         }
 
-        public IActionResult CheckIn()
-        {
-            return View();
-        }
+      
 
         public IActionResult Checkout()
         {
-            return View();
+	        var viewModel = new JoinData();
+			
+	        viewModel.Kits = _context.Kits.ToList();
+	        viewModel.Students = _context.Students.ToList();
+	        viewModel.Items = _context.Items.ToList();
+	        return View(viewModel);
         }
+
+
+        [HttpPost]
+        public IActionResult Checkout(string kitName, string studentName, string itemName)
+        {
+	        var kit = _context.Kits.FirstOrDefault(k => k.Kit_name == kitName);
+	        var student = _context.Students.FirstOrDefault(s => s.Student_fname == studentName);
+	        var item = _context.Items.FirstOrDefault(i => i.Item_name == itemName);
+	        if (kit == null && student == null && item == null)
+	        {
+		        // handle the case where no matching Kit is found
+		        return NotFound();
+	        }
+	        else
+	        {
+		        // use the selected Kit object and its properties
+		        kit.Status_code = "2";
+		        kit.Student_macid = student.Student_macid;
+		        // ...
+		        _context.SaveChanges();
+		        return RedirectToAction("Checkout");
+	        }
+        }
+
+        public IActionResult Checkin()
+        {
+	        var viewModel = new JoinData();
+			
+	        viewModel.Kits = _context.Kits.ToList();
+	        viewModel.Students = _context.Students.ToList();
+	        viewModel.Items = _context.Items.ToList();
+	        return View(viewModel);
+        }
+
+
+        [HttpPost]
+        public IActionResult Checkin(string kitName, string studentName, string itemName)
+        {
+	        var kit = _context.Kits.FirstOrDefault(k => k.Kit_name == kitName);
+	        var student = _context.Students.FirstOrDefault(s => s.Student_fname == studentName);
+	        var item = _context.Items.FirstOrDefault(i => i.Item_name == itemName);
+	        if (kit == null && student == null && item == null)
+	        {
+		        // handle the case where no matching Kit is found
+		        return NotFound();
+	        }
+	        else
+	        {
+		        // use the selected Kit object and its properties
+		        kit.Status_code = "1";
+		        kit.Student_macid = student.Student_macid;
+		        // ...
+		        _context.SaveChanges();
+		        return RedirectToAction("Checkin");
+	        }
+        }
+
+
         public IActionResult Docs()
         {
             return View();
